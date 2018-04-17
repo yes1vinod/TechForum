2e80cd6593e511c954b20610e7feb55b12157f89
vinod santhanam <yes_vinod@yahoo.com>
2018-04-16 22:50:25 -0400

    environment.ts

--- shareNlearn/src/environments/environment.ts
+++ shareNlearn/src/environments/environment.ts
@@ -7,6 +7,11 @@ import * as firebase from 'firebase/app';
 export const environment = {
   production: false,
   firebase: {
-  
+    apiKey: "AIzaSyANhgoDbFLysVmyfTRoYm3m9OXVg8YFUfA",
+    authDomain: "sharenlearn-223d0.firebaseapp.com",
+    databaseURL: "https://sharenlearn-223d0.firebaseio.com",
+    projectId: "sharenlearn-223d0",
+    storageBucket: "",
+    messagingSenderId: "553032259019"
   }
 };
